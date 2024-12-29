28b0f001fc6426f0c3067197d1e88e4520ef6a67
Mayur Lakshkar <142707712+mayur777-ui@users.noreply.github.com>
2024-12-29 13:20:07 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     const [showPassword, setShowPassword] = useState(false); // State for password visibility
 
     // Constants
-    const USER_API_END_POINT = "https://task-manager-dpl5.onrender.com/user";
+    const USER_API_END_POINT = "https://task-manager-dpl5.onrender.com/User";
     const navigate = useNavigate();
 
     // Handle form input changes
